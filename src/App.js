797c25d7219702e6472aeb7efad1baaabba35b10
sheila <sheilapg26@gmail.com>
2022-06-20 16:00:59 +0200

    Name

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Presentation from '../src/Pages/Presentation';
+
+function App() {
+  return (
+    <div>
+      <Presentation></Presentation>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Presentation/Presentation.jsx
@@ -0,0 +1,11 @@
+import styles from './Presentation.module.css';
+
+const Presentation = () => {
+    return (
+        <div className={styles.container}>
+            <p>Hi<span>,</span> <span>I'm </span><span>Sheila</span> Perez, <span>fronted</span> developer</p>
+        </div>
+    )
+}
+
+export default Presentation;
